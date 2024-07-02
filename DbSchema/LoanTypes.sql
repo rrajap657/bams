cf840365d890b0fab20d40472a253c47e7831cfc
leena-DB <you@example.com>
2024-07-02 10:11:27 +0530

    BA-11 : BAMS - Create LoanTypes Table in Database

--- DbSchema/LoanTypes.sql
+++ DbSchema/LoanTypes.sql
@@ -13,3 +13,18 @@ create table BAMS_Loan_Type_Detl(loan_type_id  NUMBER(4),
 
   create sequence seq_loan_type_id start with 1 increment by 1 maxvalue 9999;
 
+/
+COMMENT ON COLUMN BAMS_Loan_Type_Detl.loan_type_id IS ' A unique identifier for each loan type';
+/
+COMMENT ON COLUMN BAMS_Loan_Type_Detl.type_name IS 'The name of the loan type';
+/
+COMMENT ON COLUMN BAMS_Loan_Type_Detl.description   IS 'A description of the loan type';
+/
+COMMENT ON COLUMN BAMS_Loan_Type_Detl.min_credit_score  IS 'The minimum credit score required to qualify for the loan type.';
+/
+COMMENT ON COLUMN BAMS_Loan_Type_Detl.interest_rate IS 'The interest rate associated with the loan type';
+/
+
+-- End of Creating LoanTypes Table
+
+
