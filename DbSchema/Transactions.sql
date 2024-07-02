bf9d628c3846070dd20796d4fb62bc0be246a133
leena-DB <you@example.com>
2024-07-02 09:54:03 +0530

    BA-10 : BAMS - Create Transactions Table in Database

--- DbSchema/Transactions.sql
+++ DbSchema/Transactions.sql
@@ -14,4 +14,20 @@ create table BAMS_daly_tran_detl(transaction_id  NUMBER(16),
                                 constraint fk_ac_id foreign key (account_id) references Accounts(account_id),
                                 constraint fk_tran_typ_id foreign key (transaction_type_id) references TransactionTypes(transaction_type_id));
 
-select * from BAMS_daly_tran_detl;
+
+
+/
+COMMENT ON COLUMN BAMS_daly_tran_detl.transaction_id IS 'A unique identifier for each transaction';
+/
+COMMENT ON COLUMN BAMS_daly_tran_detl.account_id IS 'A foreign key that references the account_id in the Accounts table';
+/
+COMMENT ON COLUMN BAMS_daly_tran_detl.transaction_type_id  IS 'A foreign key that references the transaction_type_id in the TransactionTypes table';
+/
+COMMENT ON COLUMN BAMS_daly_tran_detl.amount IS 'The amount of the transaction.';
+/
+COMMENT ON COLUMN BAMS_daly_tran_detl.transaction_date IS 'The date and time when the transaction occurred';
+/
+COMMENT ON COLUMN BAMS_daly_tran_detl.description IS 'A description of the transaction';
+/
+
+-- End of Creating Transactions Table
